1219a333b25424c03114b3770033646ddcbccd16
adilzhanY <adilzhanit@gmail.com>
2025-07-24 01:33:25 +0200

    resume feedback components

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -19,7 +19,8 @@ const Resume = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!isLoading && !auth.isAuthenticated) navigate(`/auth?next=/resume${id}`);
+    if (!isLoading && !auth.isAuthenticated)
+      navigate(`/auth?next=/resume${id}`);
   }, [isLoading]);
 
   useEffect(() => {
@@ -77,9 +78,12 @@ const Resume = () => {
           <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
           {feedback ? (
             <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
-                <Summary feedback={feedback}/>
-                <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []}/>
-                <Details feedback={feedback}/>
+              <Summary feedback={feedback} />
+              <ATS
+                score={feedback.ATS.score || 0}
+                suggestions={feedback.ATS.tips || []}
+              />
+              <Details feedback={feedback} />
             </div>
           ) : (
             <img src="/images/resume-scan-2.gif" className="w-full" />
